6267325e47a34ed77383be8868f12369588f5af8
fabrizioradica <fabrizio.radica@gmail.com>
2021-01-26 16:54:38 +0100

    fix dead state

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 		Animator anim;
 		float movx, movz; // Variabili per memorizzare l'input del controller 
 		Rigidbody rb;
-		public float speed; // Setto la velocità di movimento 
+		public float speed; // Setto la velocitï¿½ di movimento 
 		 // Variabile che permette il movimento del mio personaggio negli assi 
 		[SerializeField] float maxspeed;
 		[SerializeField] float vel;
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour {
 	void Start () 
 	{
 		anim = gameObject.GetComponentInChildren<Animator>();// abbiamo messo l'animator dove possiamo accedere agli stati per richiamarli quando ci servono
-		rb = GetComponent<Rigidbody>(); //recupero il componente del rigidbody del mio player  perchè le informazioni siano accessibili
+		rb = GetComponent<Rigidbody>(); //recupero il componente del rigidbody del mio player  perchï¿½ le informazioni siano accessibili
 	}
 
 	void Update()
@@ -29,11 +29,11 @@ public class Player : MonoBehaviour {
 
 			if (movz != 0)
 			{
-				anim.SetBool("isidle", true);
-			} // quando è maggiore di 0 corre se è 0 o è meno va in iddle 
+				//anim.SetBool("isidle", true);
+			} // quando ï¿½ maggiore di 0 corre se ï¿½ 0 o ï¿½ meno va in iddle 
 			else
 			{
-				anim.SetBool("isidle", false);
+
 			}
 			/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -43,19 +43,26 @@ public class Player : MonoBehaviour {
 			Vector3 movimento = new Vector3(0, movx, 0); //
 			transform.Rotate(movimento); //ruotasuitre assi,creo assi nuovi non ruoto solo nell'asse y cosi si gira nel momento in cui arriva l'input del movx e movz
 										 // incremento assi va da 0.0f a 1.0f
-										 //  spinta =z*movimento*velocità 
-										 // spinta=z*0*velocità =fermo 
-										 // spinta =Z*(1*velocità)= avanti
-										 // spinta=z*(-1*velocità)= indietro
+										 //  spinta =z*movimento*velocitï¿½ 
+										 // spinta=z*0*velocitï¿½ =fermo 
+										 // spinta =Z*(1*velocitï¿½)= avanti
+										 // spinta=z*(-1*velocitï¿½)= indietro
 			movimento.y = rb.velocity.y;
 			mass = movimento * rb.mass;
-			rb.AddForce(transform.forward * (movz * speed) * Time.deltaTime, ForceMode.Acceleration); //movimento in avanti forward,incremento l'asse per andare avanti moltiplicando la velocità spingendo in avanti, delta time e do accelerazione con  la forza 
+			rb.AddForce(transform.forward * (movz * speed) * Time.deltaTime, ForceMode.Acceleration); //movimento in avanti forward,incremento l'asse per andare avanti moltiplicando la velocitï¿½ spingendo in avanti, delta time e do accelerazione con  la forza 
 																									  //vel = rb.velocity.magnitude;                                                           //prendo nella fisica velocity e magnitudo,il valore recupera l'accelerazione fa vedere a quanti kilometri orari sta andando
 			print(vel);
+			if(vel>0){
+					anim.SetBool("isidle", true);
+
+			}else{
+				anim.SetBool("isidle", false);
+
+			}
 			//rb.velocity = new Vector3(rb.velocity.x, mass.y, rb.velocity.z); //determina la massa dell'oggetto 
 
 
-			if (vel >= maxspeed) //se la velocità sara maggiore del max speed terrò la velocità costante
+			if (vel >= maxspeed) //se la velocitï¿½ sara maggiore del max speed terrï¿½ la velocitï¿½ costante
 			{
 				rb.velocity = rb.velocity.normalized * maxspeed;
 			}
@@ -71,10 +78,11 @@ public class Player : MonoBehaviour {
 		//rb.AddForce(movement * speed);
 
 		}
-	private void OnCollisionEnter(Collision collision)//con la variabile collision rileva dove va il nostro oggetto va a sbattere. Inoltre collision enter può essere scritto sono una volta 
+	private void OnCollisionEnter(Collision collision)//con la variabile collision rileva dove va il nostro oggetto va a sbattere. Inoltre collision enter puï¿½ essere scritto sono una volta 
 	{
+		print(collision.gameObject.name);
 		// print(collision.gameObject.tag);
-		if (collision.gameObject.tag == " Mostro") //se il nostro cubo va a spattere contro questo oggetto
+		if (collision.gameObject.tag == "Monster") //se il nostro cubo va a spattere contro questo oggetto
 		{
 
 			print(" DEAD ");// nel caso va a sbattere deve stampare questo
@@ -82,7 +90,7 @@ public class Player : MonoBehaviour {
 			//DEAD.text = "DEAD";
 			transform.position = Respawn.transform.position;
 			//Destroy(collision.gameObject);//distrugge l'oggetto contro cui va a sbattere 
-			//print("DEAD");// stamperà ostacolo
+			//print("DEAD");// stamperï¿½ ostacolo
 			anim.SetBool("die", true); //Se tocco il nemico muoio
 			Morto = true; 
 		}
